45b61ee175070ec1910f6215e8852effc4de9c27
Adrenalina444 <guldadelina6b2013@gmail.com>
2024-11-19 21:53:37 +0100

    Update .gitignore and remove ignored files from tracking

--- MED5_p5_VR/Assets/Scripts/PanelButton.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class PanelButton : MonoBehaviour
-{
-    public int panelIndex;  // Index of the panel to load
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# P5_VRsimulation
-VR student teacher simulation_P5
